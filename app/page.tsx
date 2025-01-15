0f5e11760ff1c9424794d6f46087ef32ee83c9b6
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2025-01-15 23:47:28 +0900

    add:全体のレイアウトの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 import { client } from '@/lib/apolloClient'
 import Button from './users/_components/Button'
 import { SEARCH_USERS } from '@/graphql/queries/SearchUsers'
+
 export default async function Home() {
-  const { data } = await client.query({
-    query: SEARCH_USERS,
-    variables: {
-      lastName: 'a'
-    },
-    fetchPolicy: 'network-only'
-  })
+  // const { data } = await client.query({
+  //   query: SEARCH_USERS,
+  //   variables: {
+  //     lastName: 'a'
+  //   },
+  //   fetchPolicy: 'network-only'
+  // })
   // const {data: hoge} = await client.mutate({mutation: REGISTER_USER})
-  console.log(data)
-  return (
-    <div>
-      <Button />
-    </div>
-  )
+  return <div></div>
 }
