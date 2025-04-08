d2997bfb7c031a090ed7f2a573c6d83cb2277ea7
Ryota Kojima <ryotakojima@RyotanoMacBook-Pro.local>
2025-04-08 13:07:09 +0900

    wip:ボタンコンポーネントの実装

--- app/users/_components/Button.tsx
+++ app/users/_components/Button.tsx
@@ -4,6 +4,7 @@ import { signin, login, logout, updateToken } from "../../actions"
 export default function Button() {
   return (
     <div>
+      {/* WIP：ボタンコンポーネントの実装 */}
       <button onClick={signin}>「サインイン」</button>
       <button onClick={login}>「ログイン」</button>
       <button onClick={logout}>「ログアウト」</button>
